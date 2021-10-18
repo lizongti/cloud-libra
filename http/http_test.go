afc227efbb1b782171d174b38ccf37050b440cb5
lizongti <lzongti@gmail.com>
2021-10-18 17:17:48 +0800

    add client_test and server

--- http/http_test.go
+++ http/http_test.go
@@ -1,7 +1,206 @@
 package http_test
 
-import "github.com/aceaura/libra/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strings"
+	"testing"
+	"time"
 
-func main() {
-	http.Client().
+	"github.com/aceaura/libra/http"
+)
+
+func TestClientGet(t *testing.T) {
+	resp, body, err := http.Get("www.baidu.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http get received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientDoGet(t *testing.T) {
+	resp, body, err := http.Do(http.GET, "www.baidu.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http get received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientPost(t *testing.T) {
+	resp, body, err := http.Post("www.baidu.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http post received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientDoPost(t *testing.T) {
+	resp, body, err := http.Do(http.POST, "www.baidu.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http post received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientHead(t *testing.T) {
+	resp, body, err := http.Head("www.baidu.com")
+	if err != nil {
+		t.Error(resp)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) > 0 {
+		t.Error("http head received body content")
+	}
+	if len(resp.Header) == 0 {
+		t.Error("http head received empty header")
+	}
+	t.Log(resp.Header)
+}
+
+func TestClientDoHead(t *testing.T) {
+	resp, body, err := http.Do(http.HEAD, "www.baidu.com")
+	if err != nil {
+		t.Error(resp)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) > 0 {
+		t.Error("http head received body content")
+	}
+	if len(resp.Header) == 0 {
+		t.Error("http head received empty header")
+	}
+	t.Log(resp.Header)
+}
+
+func TestClientParam(t *testing.T) {
+	resp, body, err := http.Get("https://top.baidu.com/board",
+		http.WithParam("platform", "pc"),
+		http.WithParam("sa", "pcindex_entry"),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientForm(t *testing.T) {
+	resp, body, err := http.NewClient().WithForm(url.Values{
+		"platform": []string{"pc"},
+		"sa":       []string{"pcindex_entry"},
+	}).Get("https://top.baidu.com/board")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientProtocol(t *testing.T) {
+	resp, body, err := http.NewClient().WithProtocol("https").Get("top.baidu.com/board?platform=pc&sa=pcindex_entry")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http get received empty body")
+	}
+	t.Log(string(body))
+}
+
+func TestClientTimeout(t *testing.T) {
+	_, _, err := http.NewClient().WithTimeout(time.Duration(1) * time.Microsecond).Get("https://top.baidu.com/board?platform=pc&sa=pcindex_entry")
+	if strings.Index(err.Error(), "context deadline exceeded") < 0 {
+		t.Error("http timeout does not work")
+	}
+}
+
+func TestContentType(t *testing.T) {
+	resp, body, err := http.NewClient().WithContentType("exception").WithRetry(3).Get("https://top.baidu.com/board?platform=pc&sa=pcindex_entry")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http received empty body")
+	}
+	if resp.Request.Header["Content-Type"][0] != "exception" {
+		t.Error("http content type does not work")
+	}
+}
+
+func TestBody(t *testing.T) {
+	http.Serve("localhost:1989",
+		http.WithBackground(true),
+		http.WithRoute("/call", func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Fprint(w, body)
+		}))
+	text := "this is a body text"
+	resp, body, err := http.NewClient().WithBody(text).Get("localhost:1989/call")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Error("http received response status not 200")
+	}
+	if len(body) == 0 {
+		t.Error("http received empty body")
+	}
+	if string(body) != text {
+		t.Error("http body does not work")
+	}
+}
+
+func TestRetry(t *testing.T) {
+	_, _, err := http.NewClient().WithTimeout(time.Duration(1) * time.Microsecond).WithRetry(3).Get("https://top.baidu.com/board?platform=pc&sa=pcindex_entry")
+	if strings.Index(err.Error(), "context deadline exceeded") < 0 {
+		t.Error("http timeout does not work")
+	}
 }
---
+++ http/server.go
@@ -0,0 +1,109 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	*serverOpt
+	router *mux.Router
+}
+
+func NewServer(options ...serverOption) *Server {
+	s := &Server{
+		serverOpt: newServerOpt(options),
+		router:    mux.NewRouter(),
+	}
+	s.doOpt(s)
+	return s
+}
+
+func Serve(addr string, options ...serverOption) error {
+	return NewServer(options...).Serve(addr)
+}
+
+func (s *Server) Serve(addr string) error {
+	s.init()
+	if s.background {
+		go s.serve(addr)
+	}
+	return s.serve(addr)
+}
+
+func (s *Server) serve(addr string) (err error) {
+	if s.safety {
+		defer func() {
+			if v := recover(); v != nil {
+				err = v.(error)
+			}
+		}()
+	}
+	return http.ListenAndServe(addr, s.router)
+}
+
+func (s *Server) init() {
+	for _, routes := range s.routes {
+		s.router.HandleFunc(routes.path, routes.handleFunc)
+	}
+}
+
+type serverOption func(*Server)
+type serverOptions []serverOption
+type route struct {
+	path       string
+	handleFunc func(http.ResponseWriter, *http.Request)
+}
+
+type serverOpt struct {
+	serverOptions
+	routes     []*route
+	background bool
+	safety     bool
+}
+
+func newServerOpt(options []serverOption) *serverOpt {
+	return &serverOpt{
+		serverOptions: options,
+	}
+}
+
+func (opt *serverOpt) doOpt(s *Server) {
+	for _, option := range opt.serverOptions {
+		option(s)
+	}
+}
+
+func WithRoute(path string, f func(http.ResponseWriter, *http.Request)) serverOption {
+	return func(s *Server) {
+		s.WithRoute(path, f)
+	}
+}
+
+func (s *Server) WithRoute(path string, f func(http.ResponseWriter, *http.Request)) *Server {
+	s.routes = append(s.routes, &route{path, f})
+	return s
+}
+
+func WithBackground(background bool) serverOption {
+	return func(s *Server) {
+		s.WithBackground(background)
+	}
+}
+
+func (s *Server) WithBackground(background bool) *Server {
+	s.background = background
+	return s
+}
+
+func WithServerSafety(safety bool) serverOption {
+	return func(s *Server) {
+		s.WithServerSafety(safety)
+	}
+}
+
+func (s *Server) WithServerSafety(safety bool) *Server {
+	s.safety = safety
+	return s
+}
