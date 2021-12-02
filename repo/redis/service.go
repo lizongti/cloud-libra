187ddd0c49d45af370ebe00d266452f7c714e1ab
lizongti <lzongti@gmail.com>
2021-12-02 12:08:32 +0800

    no thought to solve redis protocol

--- repo/redis/service.go
+++ repo/redis/service.go
@@ -6,7 +6,61 @@ import (
 	"github.com/aceaura/libra/core/device"
 )
 
+type RedisActionType int
+
+const (
+	Append RedisActionType = iota
+	BITCOUNT
+	BITOP
+	DECR
+	DECRBY
+	GET
+	GETBIT
+	GETRANGE
+	GETSET
+	INCR
+	INCRBY
+	INCRBYFLOAT
+	MGET
+	MSET
+	MSETNX
+	PSETEX
+	SET
+	SETBIT
+	SETEX
+	SETNX
+	SETRANGE
+	STRLEN
+)
+
+var redisActionName = map[RedisActionType]string{
+	Append:      "APPEND",
+	BITCOUNT:    "BITCOUNT",
+	BITOP:       "BITOP",
+	DECR:        "DECR",
+	DECRBY:      "DECRBY",
+	GET:         "GET",
+	GETBIT:      "GETBIT",
+	GETRANGE:    "GETRANGE",
+	GETSET:      "GETSET",
+	INCR:        "INCR",
+	INCRBY:      "INCRBY",
+	INCRBYFLOAT: "INCRBYFLOAT",
+	MGET:        "MGET",
+	MSET:        "MSET",
+	MSETNX:      "MSETNX",
+	PSETEX:      "PSETEX",
+	SET:         "SET",
+	SETBIT:      "SETBIT",
+	SETEX:       "SETEX",
+	SETNX:       "SETNX",
+	SETRANGE:    "SETRANGE",
+	STRLEN:      "STRLEN",
+}
+
 type ServiceRequest struct {
+	Addr string
+	Cmd  string
 }
 
 type ServiceResponse struct {
@@ -18,10 +72,10 @@ func init() {
 	device.Bus().WithService(&Service{})
 }
 
-func (d *Service) Redis(ctx context.Context, req *ServiceRequest) (resp *ServiceResponse, err error) {
+func (s *Service) Redis(ctx context.Context, req *ServiceRequest) (resp *ServiceResponse, err error) {
 	return nil, nil
 }
 
-func (d *Service) RedisPipeline(ctx context.Context, req *ServiceRequest) (resp *ServiceResponse, err error) {
+func (s *Service) RedisPipeline(ctx context.Context, req *ServiceRequest) (resp *ServiceResponse, err error) {
 	return nil, nil
 }
