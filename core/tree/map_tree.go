7ca3fb2c935f781cbe1304fe894015e1f46111e3
lizongti <lzongti@gmail.com>
2021-12-29 15:59:36 +0800

    fix map_tree

--- core/tree/map_tree.go
+++ core/tree/map_tree.go
@@ -1,9 +1,12 @@
 package tree
 
 import (
+	"errors"
 	"strconv"
+)
 
-	"github.com/aceaura/libra/core/cast"
+var (
+	ErrEmptyPath = errors.New("path cannot be empty")
 )
 
 type MapTree struct {
@@ -16,40 +19,200 @@ func NewMapTree(data map[string]interface{}) *MapTree {
 	}
 }
 
-func (mt *MapTree) Get(path []string) interface{} {
-	return mt.search(mt.data, path)
+func (mt *MapTree) Data() map[string]interface{} {
+	return mt.data
 }
 
-func (mt *MapTree) search(source interface{}, path []string) interface{} {
+func (mt *MapTree) Get(path []string) interface{} {
+	return mt.get(mt.data, path)
+}
+
+func (mt *MapTree) get(source interface{}, path []string) interface{} {
 	if len(path) == 0 {
 		return source
 	}
 
 	switch source := source.(type) {
-	case map[interface{}]interface{}:
-		next, ok := cast.ToStringMap(source)[path[0]]
-		if !ok {
-			return nil
-		}
-		return mt.search(next, path[1:])
 	case map[string]interface{}:
 		next, ok := source[path[0]]
 		if !ok {
 			return nil
 		}
-		return mt.search(next, path[1:])
+		return mt.get(next, path[1:])
 	case []interface{}:
 		index, err := strconv.Atoi(path[0])
 		if err != nil || len(source) <= index {
 			return nil
 		}
 		next := source[index]
-		return mt.search(next, path[1:])
+		return mt.get(next, path[1:])
 	default:
 		return nil
 	}
 }
 
+func (mt *MapTree) Set(path []string, v interface{}) {
+	mt.data = mt.set(mt.data, path, v).(map[string]interface{})
+}
+
+func (mt *MapTree) set(source interface{}, path []string, v interface{}) interface{} {
+	if len(path) == 0 {
+		return source
+	}
+
+	if len(path) == 1 {
+		switch source := source.(type) {
+		case map[string]interface{}:
+			source[path[0]] = v
+			return source
+		case []interface{}:
+			index, err := strconv.Atoi(path[0])
+			if err != nil {
+				return nil
+			}
+			for len(source) <= index {
+				source = append(source, nil)
+			}
+			source[index] = v
+			return source
+		default:
+			return source
+		}
+	}
+
+	switch source := source.(type) {
+	case map[string]interface{}:
+		next, ok := source[path[0]]
+		if !ok {
+			_, err := strconv.Atoi(path[1])
+			if err != nil {
+				next = make(map[string]interface{})
+			} else {
+				next = make([]interface{}, 0)
+			}
+		}
+		source[path[0]] = mt.set(next, path[1:], v)
+		return source
+	case []interface{}:
+		index, err := strconv.Atoi(path[0])
+		if err != nil {
+			return source
+		}
+		for len(source) <= index {
+			source = append(source, nil)
+		}
+		next := source[index]
+		if next == nil {
+			_, err := strconv.Atoi(path[1])
+			if err != nil {
+				next = make(map[string]interface{})
+			} else {
+				next = make([]interface{}, 0)
+			}
+		}
+		source[index] = mt.set(next, path[1:], v)
+		return source
+	default:
+		return source
+	}
+}
+
+func (mt *MapTree) Remove(path []string) {
+	mt.remove(mt.data, path)
+}
+
+func (mt *MapTree) remove(source interface{}, path []string) interface{} {
+	if len(path) == 0 {
+		return source
+	}
+
+	if len(path) == 1 {
+		switch source := source.(type) {
+		case map[string]interface{}:
+			source[path[0]] = nil
+			for k, v := range source {
+				if v == nil {
+					delete(source, k)
+				}
+			}
+			if len(source) == 0 {
+				return nil
+			}
+			return source
+		case []interface{}:
+			index, err := strconv.Atoi(path[0])
+			if err != nil || len(source) <= index {
+				return source
+			}
+			source[index] = nil
+			for i := len(source) - 1; i >= 0; i-- {
+				if source[i] == nil {
+					source = source[:len(source)-1]
+				} else {
+					break
+				}
+			}
+			if len(source) == 0 {
+				return nil
+			}
+			return source
+		default:
+			return source
+		}
+	}
+
+	switch source := source.(type) {
+	case map[string]interface{}:
+		next, ok := source[path[0]]
+		if !ok {
+			return source
+		}
+		source[path[0]] = mt.remove(next, path[1:])
+		for k, v := range source {
+			if v == nil {
+				delete(source, k)
+			}
+		}
+		if len(source) == 0 {
+			return nil
+		}
+		return source
+	case []interface{}:
+		index, err := strconv.Atoi(path[0])
+		if err != nil || len(source) <= index {
+			return source
+		}
+		next := source[index]
+		source[index] = mt.remove(next, path[1:])
+		for i := len(source) - 1; i >= 0; i-- {
+			if source[i] == nil {
+				source = source[:len(source)-1]
+			} else {
+				break
+			}
+		}
+		if len(source) == 0 {
+			return nil
+		}
+		return source
+	default:
+		return source
+	}
+}
+
+// Override override data from omt to mt as a new mapTree
 func (mt *MapTree) Override(omt *MapTree) *MapTree {
+	// src override dst
+	// srcData := deepcopy.Copy(omt.data)
+	// dstData := mt.data
+
+	// for sk, sv := range srcData {
+	// 	dv, ok := dstData[sk]
+	// 	if !ok {
+	// 		dstData[sk] = sv
+	// 	}
+	// }
+
+	// return NewMapTree(data)
 	return nil
 }
--- core/tree/tree_test.go
+++ core/tree/tree_test.go
@@ -24,49 +24,111 @@ type Animal struct {
 	Count       int
 }
 
-var zoo = Zoo{
-	Name: "Beasts & Montsters",
-	Keepers: []Keeper{
-		{
-			Name: "Alice",
-			Age:  20,
+func TestGetSet(t *testing.T) {
+	var zoo = Zoo{
+		Name: "Beasts & Monsters",
+		Keepers: []Keeper{
+			{
+				Name: "Alice",
+				Age:  20,
+			},
+			{
+				Name: "Tony",
+				Age:  50,
+			},
 		},
-		{
-			Name: "Tony",
-			Age:  50,
+		Animals: []Animal{
+			{
+				SpeciesName: "Elephant",
+				Count:       3,
+			},
+			{
+				SpeciesName: "Money",
+				Count:       20,
+			},
 		},
-	},
-	Animals: []Animal{
-		{
-			SpeciesName: "Elephant",
-			Count:       3,
-		},
-		{
-			SpeciesName: "Money",
-			Count:       20,
-		},
-	},
-}
+	}
 
-func TestGet(t *testing.T) {
-	zooJSON, err := encoding.Encode(encoding.NewJSON(), zoo)
+	json := encoding.NewJSON()
+	zooJSON, err := json.Marshal(zoo)
 	if err != nil {
 		t.Fatal(err)
 	}
 	var data map[string]interface{}
-	err = encoding.Decode(encoding.NewJSON(), zooJSON, &data)
+	err = json.Unmarshal(zooJSON, &data)
 	if err != nil {
 		t.Fatal(err)
 	}
+	mapTree := tree.NewMapTree(data)
 
 	style := magic.NewChainStyle(magic.SeparatorPeriod, magic.SeparatorUnderscore)
-	chain := style.Chain("animals.0.species_name")
-	mapTree := tree.NewMapTree(data)
-	t.Log(mapTree.Get(chain))
-	v := mapTree.Get(chain)
+
+	v := mapTree.Get(style.Chain("animals.0.species_name"))
 	if s, ok := v.(string); !ok {
 		t.Fatalf("expected a string, but got %v", v)
 	} else if s != "Elephant" {
 		t.Fatalf("expected a string of `Elephant`, but got %s", s)
+	}
+
+	mapTree.Set(style.Chain("name"), "Sea World")
+	mapTree.Set(style.Chain("animals.0.species_name"), "Dolphin")
+	mapTree.Set(style.Chain("keepers.3"), map[string]interface{}{"Name": "Mike", "Age": 29})
+	mapTree.Set(style.Chain("animals.4.1.3"), "a mistake")
+
+	if mapTree.Get(style.Chain("name")) != "Sea World" {
+		t.Fatalf("expected zoo name `Sea World`, but got %s", mapTree.Get(style.Chain("sea")))
+	}
+	if mapTree.Get(style.Chain("animals.0.species_name")) != "Dolphin" {
+		t.Fatalf("expected species name `Dolphin`, but got %s", mapTree.Get(style.Chain("animals.0.species_name")))
+	}
+	if mapTree.Get(style.Chain("keepers.3.name")) != "Mike" {
+		t.Fatalf("expected keeper name `Mike`, but got %s", mapTree.Get(style.Chain("keepers.3.name")))
+	}
+
+	zooJSON, err = json.Marshal(mapTree.Data())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", string(zooJSON))
+
+	mapTree.Remove(style.Chain("animals.4.1.3"))
+	mapTree.Remove(style.Chain("Keepers.1.Name"))
+	mapTree.Remove(style.Chain("Keepers.1.Age"))
+	zooJSON, err = json.Marshal(mapTree.Data())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", string(zooJSON))
+	zoo = Zoo{}
+	err = json.Unmarshal(zooJSON, &zoo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", zoo)
+	if zoo.Name != "Sea World" {
+		t.Fatalf("expected species name `Sea World`, but got %s", zoo.Name)
+	}
+	if zoo.Animals[0].SpeciesName != "Dolphin" {
+		t.Fatalf("expected species name `Dolphin`, but got %s", zoo.Animals[0].SpeciesName)
+	}
+	if zoo.Keepers[3].Name != "Mike" {
+		t.Fatalf("expected keeper name `Mike`, but got %s", zoo.Keepers[3].Name)
+	}
+
+	mapTree.Remove(style.Chain("Keepers.3.Name"))
+	mapTree.Remove(style.Chain("Keepers.3.Age"))
+	zooJSON, err = json.Marshal(mapTree.Data())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", string(zooJSON))
+	zoo = Zoo{}
+	err = json.Unmarshal(zooJSON, &zoo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%+v", zoo)
+	if len(zoo.Keepers) != 1 {
+		t.Fatalf("expected 1 keeper, but got %d", len(zoo.Keepers))
 	}
 }
