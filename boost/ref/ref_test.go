4efeaed71be4fa3ac2661b406809a8c9c7702b37
lizongti <lzongti@gmail.com>
2022-02-18 18:29:56 +0800

    fix ref

--- boost/ref/ref_test.go
+++ boost/ref/ref_test.go
@@ -6,13 +6,22 @@ import (
 	"testing"
 )
 
-type T struct{}
+type T struct {
+	T2
+}
 
 func (t *T) Foo() {
 	fmt.Println("foo")
 }
 
+type T2 struct{}
+
+func (t *T2) Bar() {
+	fmt.Println("bar")
+}
+
 func TestCallName(t *testing.T) {
 	var o T
 	reflect.ValueOf(&o).MethodByName("Foo").Call([]reflect.Value{})
+	reflect.ValueOf(&o).MethodByName("Bar").Call([]reflect.Value{})
 }
