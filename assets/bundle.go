983375c22c5e17a0cbc6042e3baca7ee795eea36
lizongti <lzongti@gmail.com>
2022-09-19 18:30:17 +0800

    add bundle get

--- assets/bundle.go
+++ assets/bundle.go
@@ -2,66 +2,87 @@ package assets
 
 import (
 	"crypto/md5"
+	"errors"
 	"fmt"
 	"path/filepath"
 )
 
-type ProviderBundle struct {
-	provider Provider
-	path     string
+var ErrorBundleNotFound = errors.New("bundle not found")
+
+// Bundle get assets from multiple providers.
+// If any bundle is in specific provider, this bundle should be complected.
+// If not, unexpected error would occur.
+type Bundle struct {
+	providers []Provider
 }
 
-func NewProviderBundle(provider Provider, path string) *ProviderBundle {
-	return &ProviderBundle{
-		provider: provider,
-		path:     path,
+// NewBundle returns a new bundle.
+func NewBundle(provider ...Provider) *Bundle {
+	return &Bundle{
+		providers: provider,
 	}
 }
 
-func (pb *ProviderBundle) Get(path string) ([]byte, error) {
-	dataMap := make(map[string][]byte)
+func (b *Bundle) Get(path string) (map[string][]byte, error) {
+	for _, provider := range b.providers {
+		assetMap, err := b.getFromProvider(path, provider)
+		if err != nil {
+			return nil, err
+		}
 
-	if err := pb.getRecursive(pb.path, dataMap); err != nil {
+		if len(assetMap) > 0 {
+			return assetMap, nil
+		}
+	}
+
+	return nil, fmt.Errorf("%w: %s", ErrorBundleNotFound, path)
+}
+
+// Get returns the asset with the given name.
+func (b *Bundle) getFromProvider(path string, provider Provider) (map[string][]byte, error) {
+	assetMap := make(map[string][]byte)
+
+	if err := b.getFromProviderRecursive(path, provider, assetMap); err != nil {
 		return nil, err
 	}
 
-	return dataMap[path], nil
+	return assetMap, nil
 }
 
-func (pb *ProviderBundle) getRecursive(path string, dataMap map[string][]byte) error {
+func (b *Bundle) getFromProviderRecursive(path string, provider Provider, assetMap map[string][]byte) error {
 	// Get names in path
-	names, err := pb.provider.List(path)
+	names, err := provider.List(path)
 	if err != nil {
 		return err
 	}
 
 	// Name has no sub assets
 	if len(names) == 0 {
-		data, err := pb.provider.Get(path)
+		data, err := provider.Get(path)
 		if err != nil {
 			return err
 		}
 
-		dataMap[path] = data
+		assetMap[path] = data
 
 		return nil
 	}
 
 	// Name has sub assets
 	for _, name := range names {
-		if err := pb.getRecursive(filepath.Join(path, name), dataMap); err != nil {
+		if err := b.getFromProviderRecursive(filepath.Join(path, name), provider, assetMap); err != nil {
 			return err
 		}
 	}
 
 	// Add self to dataMap
-	data, err := pb.provider.Get(path)
+	data, err := provider.Get(path)
 	if err != nil {
 		return err
 	}
 
 	cryptName := fmt.Sprintf("%x", md5.Sum([]byte(path)))
-	dataMap[filepath.Join(path, cryptName)] = data
+	assetMap[filepath.Join(path, cryptName)] = data
 
 	return nil
 }
